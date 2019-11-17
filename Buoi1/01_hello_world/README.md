e94bbe3c0e1659cad83afbf3cebf8546fa9419ae
Taof <taof@Taofs-MacBook-Pro.local>
2019-11-17 10:31:47 +0700

    Buổi 1

---
+++ Buoi1/01_hello_world/README.md
@@ -0,0 +1,10 @@
+# hello_world
+
+Ứng dụng hiển thị ra màn hình câu: "Chào thế giới"
+
+- Ứng dụng Flutter luôn phải trả về một Material App
+- Material App này có thuộc tính:
+    - title: Tên app
+    - home: Quy định màn chạy đầu tiên
+
+![Hyper-V-2](../img/helloworld.png)
---
+++ Buoi1/01_hello_world/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+// Một ứng dụng Flutter bắt buộc phải trả về đầu tiên là một MaterialApp,
+// MaterialApp này sẽ có thuộc tính title (Tên ứng dụng hiển thị cho người dùng) và home (quy định màn đầu tiên xuất hiện)
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: HelloWorld(),
+    );
+  }
+}
+
+class HelloWorld extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [Colors.teal, Colors.blue])),
+                child: Center( 
+                  child: Text('Hello World!', style: TextStyle(fontSize: 40, color: Colors.white),),
+                ),
+      ),
+    );
+  }
+}
